6f5d94cd3217989a89d0536a2c3fa5baa1813706
LaviIonas <laviionas772@gmail.com>
2019-02-25 20:04:01 +0000

    fixed bug where error mesages would stay after tweeting

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,51 +1,4 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-const data = [{
-    "user": {
-        "name": "Newton",
-        "avatars": {
-            "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-            "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-            "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-    },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-}, {
-    "user": {
-        "name": "Descartes",
-        "avatars": {
-            "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-            "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-            "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd"
-    },
-    "content": {
-        "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-}, {
-    "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-            "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-            "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-            "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-    },
-    "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-}];
+
 
 var createTweetElement = function(tweetData) {
     console.log(tweetData);
@@ -129,7 +82,6 @@ function loadTweets() {
 }
 $(document).ready(() => {
     loadTweets();
-
     var $form = $('#form');
     $form.on('submit', function(event) {
         event.preventDefault();
@@ -139,29 +91,24 @@ $(document).ready(() => {
         console.log(serForm);
         if ($form.find("#field").val() === "") {
             $("#empty").show();
-        }else if ($form.find("#field").val().length > 140) {
+            $("#tooLong").hide();
+        } else if ($form.find("#field").val().length > 140) {
             $("#tooLong").show();
-        } else {
+            $("#empty").hide();
+        }
+        if (!($form.find("#field").val() === "") && !($form.find("#field").val().length > 140)) {
             $.post("/tweets", serForm);
             $(".new-tweet").slideToggle("slow");
             $("#empty").hide();
-        $("#tooLong").hide();
+            $("#tooLong").hide();
         }
         loadTweets();
-
-
     });
-
-
-
-
     var $button = $('#compose');
     $button.on('click', function(event) {
         event.preventDefault();
-
         $(".new-tweet").slideToggle("slow");
         $(".new-tweet textarea").focus();
-
     });
 });
 // to add it to the page so we can make sure it's got all the right elements, classes, etc.
